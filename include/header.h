f57210688422353c1731ef764cdb0e58b7e7c023
LemuriiL <nice.duble@mail.ru>
2021-07-05 16:28:46 -0800

    first

---
+++ include/header.h
@@ -0,0 +1,9 @@
+#ifndef TEMPLATE_HEADER_H
+#define TEMPLATE_HEADER_H
+#include <string>
+#include <vector>
+#include <iostream>
+using string = std::string;
+using std::cout;
+using std::endl;
+#endif // TEMPLATE_HEADER_H
---
+++ sources/Histogram.cpp
@@ -0,0 +1,20 @@
+#include "Histogram.h"
+int Histogram::GetNumSkip() const {
+  return num_skip;
+}
+Histogram& Histogram::GetInstance() {
+  static Histogram instance;
+  return instance;
+}
+void Histogram::SetAvg(const float& avg_) {
+  avg = avg_;
+}
+void Histogram::PlusNumSkip() {
+  ++num_skip;
+}
+void Histogram::NewLap() {
+  num_skip = 0;
+}
+float Histogram::GetAvg() const {
+  return avg;
+}
